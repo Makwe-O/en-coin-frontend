3ae058b3b7a575ee037725ca63d2ede841509c93
onyeka mmakwe <notmaks@yahoo.com>
2019-11-16 16:36:40 +0100

    change folder name

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Nav from '../../components/nav/Nav';
 import BuyCrypto from '../../components/buyCrypto/BuyCrypto';
-import EarnCrypto from '../../components/earnCrypto/EarnCrypto';
+import EarnCrypto from '../../components/learnCrypto/EarnCrypto';
 import CreatePorfolio from '../../components/createPortfolio/CreatePorfolio';
 import TrustedPlatform from '../../components/trustedPlatform/TrustedPlatform';
 import Banner from '../../components/banner/Banner';
 import GetStarted from '../../components/getStarted/GetStarted';
-import EarnCryptoBanner from '../../components/earnCryptoBanner/EarnCryptoBanner';
+import EarnCryptoBanner from '../../components/learnCrypto/EarnCrypto';
 import Footer from '../../components/footer/Footer';
 const LandingPage = () => {
   return (
